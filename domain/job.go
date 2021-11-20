afbcb64a425c6ffb9a473de8d16b63ddf3730c52
Lucas Saraiva <lucassaraiva5@hotmail.com>
2021-11-20 16:59:17 -0300

    Creating test for entity job

--- domain/job.go
+++ domain/job.go
@@ -1,14 +1,56 @@
 package domain
 
-import "time"
+import (
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+	"time"
+)
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
 
 type Job struct {
-	ID string
-	OutputBucketPath string
-	Status string
-	Video *Video
-	VideoId string
-	Error string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID string `valid:"uuid"`
+	OutputBucketPath string `valid:"notnull"`
+	Status string `valid:"notnull"`
+	Video *Video `valid:"-"`
+	VideoId string `valid:"-"`
+	Error string `valid:"-"`
+	CreatedAt time.Time `valid:"-"`
+	UpdatedAt time.Time `valid:"-"`
+}
+
+func NewJob(output string, status string, video *Video) (*Job, error) {
+	job := Job{
+		OutputBucketPath: output,
+		Status: status,
+		Video: video,
+	}
+
+	job.prepare()
+
+	err := job.Validate()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &job, nil
+}
+
+func (job *Job) prepare()  {
+	job.ID = uuid.NewV4().String()
+	job.CreatedAt = time.Now()
+	job.UpdatedAt = time.Now()
+}
+
+func (job *Job) Validate() error {
+	_, err := govalidator.ValidateStruct(job)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
---
+++ domain/job_test.go
@@ -0,0 +1,21 @@
+package domain_test
+
+import (
+	"encoder/domain"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestNewJob(t *testing.T) {
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	job, err := domain.NewJob("path", "converted", video)
+
+	require.NotNil(t, job)
+	require.Nil(t, err)
+}
